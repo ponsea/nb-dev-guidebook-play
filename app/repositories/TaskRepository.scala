d39354618cb51537634a9f22c4387a68ee99ac81
ponsea <puodsea@gmail.com>
2018-11-28 14:29:53 +0900

    Add - TaskRepository#save

--- app/repositories/TaskRepository.scala
+++ app/repositories/TaskRepository.scala
@@ -6,4 +6,5 @@ import scala.concurrent.Future
 trait TaskRepository {
   def findAll(): Future[Seq[Task]]
   def findById(taskId: TaskId): Future[Option[Task]]
+  def save(task: Task): Future[Task]
 }
--- app/repositories/TaskRepositoryOnMemoryImpl.scala
+++ app/repositories/TaskRepositoryOnMemoryImpl.scala
@@ -8,7 +8,7 @@ import models.{Task, TaskId, UserId}
 @Singleton()
 class TaskRepositoryOnMemoryImpl extends TaskRepository {
   private val dateTime = LocalDateTime.parse("2019-04-01T00:00:00")
-  private val tasks = Set(
+  private var tasks = Set(
     Task(TaskId("1"), "Scalaの勉強", true, UserId("1"), Some(dateTime), dateTime, dateTime),
     Task(TaskId("2"), "Playの勉強", false, UserId("1"), Some(dateTime), dateTime, dateTime),
     Task(TaskId("3"), "英語の勉強", false, UserId("1"), None, dateTime, dateTime),
@@ -20,5 +20,12 @@ class TaskRepositoryOnMemoryImpl extends TaskRepository {
 
   def findById(taskId: TaskId) = {
     Future.successful(tasks.find(_.id == taskId))
+  }
+
+  def save(newTask: Task) = {
+    val maybeOldTask = tasks.find(_.id == newTask.id)
+    maybeOldTask.foreach(old => tasks -= old)
+    tasks += newTask
+    Future.successful(newTask)
   }
 }
