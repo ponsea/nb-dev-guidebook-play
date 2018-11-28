817a69347270e3ae59adc4c65b8e7c338715c44a
ponsea <puodsea@gmail.com>
2018-11-28 11:27:23 +0900

    Add - TaskService#getAll

--- app/services/TaskService.scala
+++ app/services/TaskService.scala
@@ -6,6 +6,8 @@ import repositories.TaskRepository
 
 @Singleton()
 class TaskService @Inject()(taskRepository: TaskRepository) {
+  def getAll() = taskRepository.findAll()
+
   def get(id: String) = taskRepository.findById(TaskId(id))
 }
 
