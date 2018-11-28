6a6fae07b202a7a2427119d9ba12efb217c35366
ponsea <puodsea@gmail.com>
2018-11-28 14:45:10 +0900

    Add - TaskController#create

--- app/controllers/TaskController.scala
+++ app/controllers/TaskController.scala
@@ -1,12 +1,13 @@
 package controllers
 
 import javax.inject._
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{Future, ExecutionContext}
 import play.api._
 import play.api.mvc._
 import play.api.libs.json._
-import models.Task
+import models.{Task, UserId}
 import services.TaskService
+import json.TaskCreatingInput
 
 @Singleton
 class TaskController @Inject()(taskService: TaskService, cc: ControllerComponents)(
@@ -24,6 +25,21 @@ class TaskController @Inject()(taskService: TaskService, cc: ControllerComponent
     taskService.get(id).map { maybeTask: Option[Task] =>
       maybeTask.fold[Result](NotFound) { task: Task =>
         Ok(Json.toJson(task))
+      }
+    }
+  }
+
+  def create() = Action.async(parse.json) { request: Request[JsValue] =>
+    val inputResult: JsResult[TaskCreatingInput] = request.body.validate[TaskCreatingInput]
+    inputResult match {
+      case JsSuccess(input, _) => {
+        // TODO: UserIdをセッション情報から取得する
+        taskService.create(input.name, input.deadline, UserId("1")).map { task =>
+          Ok(Json.toJson(task))
+        }
+      }
+      case JsError(errors) => {
+        Future.successful(BadRequest(JsError.toJson(errors)))
       }
     }
   }
