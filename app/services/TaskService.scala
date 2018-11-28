8fd5744d757e552bc62c51f41a55ffe7d0f8da62
ponsea <puodsea@gmail.com>
2018-11-28 14:40:15 +0900

    Add - TaskService#create

--- app/services/TaskService.scala
+++ app/services/TaskService.scala
@@ -1,13 +1,21 @@
 package services
 
 import javax.inject.{Inject, Singleton}
-import models.TaskId
+import java.time.LocalDateTime
+import models.{Task, TaskId, UserId}
 import repositories.TaskRepository
+import utils.{IdGenerator, SystemDateTime}
 
 @Singleton()
 class TaskService @Inject()(taskRepository: TaskRepository) {
   def getAll() = taskRepository.findAll()
 
   def get(id: String) = taskRepository.findById(TaskId(id))
-}
 
+  def create(name: String, deadline: Option[LocalDateTime], userId: UserId)(
+      implicit idGen: IdGenerator,
+      sdt: SystemDateTime) = {
+    val newTask = Task(TaskId.newId(), name, false, userId, deadline, sdt.now(), sdt.now())
+    taskRepository.save(newTask)
+  }
+}
