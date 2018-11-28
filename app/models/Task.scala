3f5fac1e32fbeb7bcf25a7dbdd6130cb558b4b9f
ponsea <puodsea@gmail.com>
2018-11-28 14:25:53 +0900

    Add - TaskId0newId

--- app/models/Task.scala
+++ app/models/Task.scala
@@ -1,6 +1,7 @@
 package models
 
 import java.time.LocalDateTime
+import utils.IdGenerator
 
 case class Task(id: TaskId,
                 name: String,
@@ -11,3 +12,7 @@ case class Task(id: TaskId,
                 updatedAt: LocalDateTime)
 
 case class TaskId(value: String) extends AnyVal
+
+object TaskId {
+  def newId()(implicit idGen: IdGenerator) = TaskId(idGen.generateId())
+}
