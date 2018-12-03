d4dbc84673b5ed53387b451d99ad206e911961e7
ponsea <puodsea@gmail.com>
2018-12-03 17:34:06 +0900

    Add - TaskUpdatingInput

---
+++ app/json/TaskUpdatingInput.scala
@@ -0,0 +1,18 @@
+package json
+
+import java.time.LocalDateTime
+import play.api.libs.functional.syntax._
+import play.api.libs.json._
+import play.api.libs.json.Reads._
+
+case class TaskUpdatingInput(name: Option[String],
+                             isFinished: Option[Boolean],
+                             deadline: Option[Option[LocalDateTime]])
+
+object TaskUpdatingInput {
+  implicit val reads = (
+    (JsPath \ "name").readNullable[String](minLength[String](1) keepAnd maxLength[String](256))
+      and (JsPath \ "isFinished").readNullable[Boolean]
+      and (JsPath \ "deadline").readOptional[LocalDateTime]
+  )(apply _)
+}
