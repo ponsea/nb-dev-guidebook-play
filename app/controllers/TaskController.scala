ed09ce982165a7553ff9ba725fb0a00a29a36a3b
ponsea <puodsea@gmail.com>
2018-12-03 18:01:38 +0900

    Add - TaskController#update

--- app/controllers/TaskController.scala
+++ app/controllers/TaskController.scala
@@ -6,14 +6,16 @@ import play.api._
 import play.api.mvc._
 import play.api.libs.json._
 import models.{Task, TaskId, UserId}
-import services.TaskService
-import json.TaskCreatingInput
-import json.{TaskWrites, UserWrites}
+import services.{TaskService, ServiceError, TaskNotFound, UnauthorizedAction}
+import json._
 
 @Singleton
 class TaskController @Inject()(taskService: TaskService, cc: ControllerComponents)(
     implicit ec: ExecutionContext)
-    extends AbstractController(cc) with UserWrites with TaskWrites {
+    extends AbstractController(cc)
+    with UserWrites
+    with TaskWrites
+    with ServiceErrorWrites {
 
   def index() = Action.async {
     taskService.getAll().map { tasks =>
@@ -41,6 +43,34 @@ class TaskController @Inject()(taskService: TaskService, cc: ControllerComponent
       case JsError(errors) => {
         Future.successful(BadRequest(JsError.toJson(errors)))
       }
+    }
+  }
+
+  def update(id: String) = Action.async(parse.json) { request =>
+    val inputResult = request.body.validate[TaskUpdatingInput]
+    inputResult match {
+      case JsSuccess(input, _) => {
+        // TODO: UserIdをセッション情報から取得する
+        taskService
+          .update(TaskId(id), input.name, input.isFinished, input.deadline, UserId("1"))
+          .map { result =>
+            result match {
+              case Right(task) => Ok(Json.toJson(task))
+              case Left(error) => appropriateErrorStatusOf(error)
+            }
+          }
+      }
+      case JsError(errors) => {
+        Future.successful(BadRequest(JsError.toJson(errors)))
+      }
+    }
+  }
+
+  private def appropriateErrorStatusOf(error: ServiceError) = {
+    error match {
+      case e: TaskNotFound            => NotFound(Json.toJson(error))
+      case e: UnauthorizedAction.type => Forbidden(Json.toJson(error))
+      case e                          => BadRequest(Json.toJson(error))
     }
   }
 }
