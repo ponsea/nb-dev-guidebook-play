033900f90092f1232b5bf11ecc788cdaeb09dc96
ponsea <puodsea@gmail.com>
2018-12-09 20:59:38 +0900

    Update - TaskRepositorySlickImplをDIするように

--- app/Module.scala
+++ app/Module.scala
@@ -1,11 +1,11 @@
 import com.google.inject.AbstractModule
 import com.google.inject.name.Names
-import repositories.{TaskRepository, TaskRepositoryOnMemoryImpl}
+import repositories.{TaskRepository, TaskRepositorySlickImpl}
 import utils.{IdGenerator, SystemDateTime, IdGeneratorImpl, SystemDateTimeImpl}
 
 class Module extends AbstractModule {
   def configure() = {
-    bind(classOf[TaskRepository]).to(classOf[TaskRepositoryOnMemoryImpl])
+    bind(classOf[TaskRepository]).to(classOf[TaskRepositorySlickImpl])
     bind(classOf[IdGenerator]).to(classOf[IdGeneratorImpl])
     bind(classOf[SystemDateTime]).to(classOf[SystemDateTimeImpl])
   }
