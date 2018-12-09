a5ba65a00c1cbe2f4e76198096901aa9954cf3d1
ponsea <puodsea@gmail.com>
2018-12-09 20:55:25 +0900

    Add - TasksTableConfig

---
+++ app/repositories/db/TasksTableConfig.scala
@@ -0,0 +1,24 @@
+package repositories.db
+
+import play.api.db.slick.HasDatabaseConfigProvider
+import slick.jdbc.JdbcProfile
+import java.time.LocalDateTime
+import models.{Task, TaskId, UserId}
+
+trait TasksTableConfig extends ColumnTypeMapping {
+  self: HasDatabaseConfigProvider[JdbcProfile] =>
+  import profile.api._
+
+  class TasksTable(tag: Tag) extends Table[Task](tag, "tasks") {
+    def id = column[TaskId]("id", O.PrimaryKey)
+    def name = column[String]("name")
+    def isFinished = column[Boolean]("is_finished")
+    def userId = column[UserId]("user_id")
+    def deadline = column[Option[LocalDateTime]]("deadline")
+    def updatedAt = column[LocalDateTime]("updated_at")
+    def createdAt = column[LocalDateTime]("created_at")
+
+    def * =
+      (id, name, isFinished, userId, deadline, updatedAt, createdAt) <> (Task.tupled, Task.unapply)
+  }
+}
