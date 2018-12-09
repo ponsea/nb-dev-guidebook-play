744c676d646b4120617c62e3e77bfd3c9140abf8
ponsea <puodsea@gmail.com>
2018-12-09 20:58:50 +0900

    Add - TaskRepositorySlickImplの型注釈ついたバージョンのメソッド

--- app/repositories/TaskRepositorySlickImpl.scala
+++ app/repositories/TaskRepositorySlickImpl.scala
@@ -6,7 +6,12 @@ import scala.concurrent.{Future, ExecutionContext}
 import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
 import models.{Task, TaskId, UserId}
 import db.TasksTableConfig
+
 import slick.jdbc.JdbcProfile
+import slick.jdbc.JdbcBackend
+import slick.sql.SqlAction
+import slick.sql.FixedSqlAction
+import slick.sql.FixedSqlStreamingAction
 
 @Singleton()
 class TaskRepositorySlickImpl @Inject()(protected val dbConfigProvider: DatabaseConfigProvider)(
@@ -34,5 +39,34 @@ class TaskRepositorySlickImpl @Inject()(protected val dbConfigProvider: Database
   def delete(taskId: TaskId) = {
     val action = tasks.filter(_.id === taskId).delete
     db.run(action).map(_ => ())
+  }
+
+  // 型注釈つけて分かりやすくした場合 ----------------------
+
+  def findAllVerbose() = {
+    val action: FixedSqlStreamingAction[Seq[Task], Task, Effect.Read] = tasks.result
+    val database: JdbcBackend#DatabaseDef = db
+    val result: Future[Seq[Task]] = database.run(action)
+    result
+  }
+
+  def findByIdVerbose(taskId: TaskId) = {
+    val query: Query[TasksTable, Task, Seq] = tasks.filter(_.id === taskId)
+    val action: FixedSqlStreamingAction[Seq[Task], Task, Effect.Read] = query.result
+    val headOptionAction: SqlAction[Option[Task], NoStream, Effect.Read] = action.headOption
+    val result: Future[Option[Task]] = db.run(headOptionAction)
+    result
+  }
+
+  def saveVerbose(task: Task) = {
+    val action: FixedSqlAction[Int, NoStream, Effect.Write] = tasks.insertOrUpdate(task)
+    val result: Future[Int] = db.run(action)
+    result.map(_ => task)
+  }
+
+  def deleteVerbose(taskId: TaskId) = {
+    val action: FixedSqlAction[Int, NoStream, Effect.Write] = tasks.filter(_.id === taskId).delete
+    val result: Future[Int] = db.run(action)
+    result.map(_ => ())
   }
 }
