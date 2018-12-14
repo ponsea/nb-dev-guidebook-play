d78b1970497c48d08fbd3f42043f68a38dbc11e3
ponsea <puodsea@gmail.com>
2018-12-14 12:15:18 +0900

    Add - UserRepositoryのDI設定

--- app/Module.scala
+++ app/Module.scala
@@ -1,10 +1,11 @@
 import com.google.inject.AbstractModule
 import com.google.inject.name.Names
-import persistence.{TaskRepository, TaskRepositorySlickImpl}
-import utils.{IdGenerator, SystemDateTime, IdGeneratorImpl, SystemDateTimeImpl}
+import persistence._
+import utils._
 
 class Module extends AbstractModule {
   def configure() = {
+    bind(classOf[UserRepository]).to(classOf[UserRepositoryOnMemoryImpl])
     bind(classOf[TaskRepository]).to(classOf[TaskRepositorySlickImpl])
     bind(classOf[IdGenerator]).to(classOf[IdGeneratorImpl])
     bind(classOf[SystemDateTime]).to(classOf[SystemDateTimeImpl])
