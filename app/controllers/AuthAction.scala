359949c73ff84bb8a0e815f694fe04b6a57f6713
ponsea <puodsea@gmail.com>
2018-12-18 20:42:39 +0900

    Update - AuthRequestをMessagesRequest相当にする

--- app/controllers/AuthAction.scala
+++ app/controllers/AuthAction.scala
@@ -3,14 +3,23 @@ package controllers
 import javax.inject.Inject
 import scala.concurrent.{Future, ExecutionContext}
 import play.api.mvc._
+import play.api.i18n.MessagesApi
 import play.api.libs.json.Json
 import models.UserId
 import services.AuthService
 
-class AuthRequest[A](val userId: UserId, request: Request[A]) extends WrappedRequest[A](request)
+trait AuthRequestHeader extends MessagesRequestHeader with PreferredMessagesProvider {
+  def userId: UserId
+}
 
-class AuthAction @Inject()(val parser: BodyParsers.Default, authService: AuthService)(
-    implicit val executionContext: ExecutionContext)
+class AuthRequest[A](val userId: UserId, val messagesApi: MessagesApi, request: Request[A])
+    extends WrappedRequest[A](request)
+    with AuthRequestHeader
+
+class AuthAction @Inject()(
+    val parser: BodyParsers.Default,
+    messagesApi: MessagesApi,
+    authService: AuthService)(implicit val executionContext: ExecutionContext)
     extends ActionBuilder[AuthRequest, AnyContent]
     with ActionRefiner[Request, AuthRequest]
     with Results {
@@ -18,7 +27,7 @@ class AuthAction @Inject()(val parser: BodyParsers.Default, authService: AuthSer
   def refine[A](request: Request[A]) = Future.successful {
     request.session
       .get(authService.SESSION_KEY)
-      .map(uid => new AuthRequest(UserId(uid), request))
+      .map(uid => new AuthRequest(UserId(uid), messagesApi, request))
       .toRight(Unauthorized(Json.obj("message" -> "authentication required")))
   }
 }
