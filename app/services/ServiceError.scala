2429d77f21b4943b367c27d7cf5b624e400ddc90
ponsea <puodsea@gmail.com>
2018-12-22 20:26:00 +0900

    Add - UserError

--- app/services/ServiceError.scala
+++ app/services/ServiceError.scala
@@ -7,3 +7,6 @@ abstract class ServiceError(val message: String, val args: Any*)
 abstract sealed class TaskError(message: String, args: Any*) extends ServiceError(message, args: _*)
 case class TaskNotFound(taskId: TaskId) extends TaskError("error.task.notFound", taskId)
 case object TaskPermissionDenied extends TaskError("error.task.permissionDenied")
+
+abstract sealed class UserError(message: String, args: Any*) extends ServiceError(message, args: _*)
+case class UserEmailDuplicated(email: String) extends UserError("error.user.emailDuplicated", email)
