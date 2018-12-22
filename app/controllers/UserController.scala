679fa6b0804d91ab4152a150c87ad51a91fb22f7
ponsea <puodsea@gmail.com>
2018-12-22 20:31:44 +0900

    Add - UserController#create

--- app/controllers/UserController.scala
+++ app/controllers/UserController.scala
@@ -14,7 +14,8 @@ class UserController @Inject()(userService: UserService,
                                authAction: AuthAction,
                                cc: ControllerComponents)(implicit ec: ExecutionContext)
     extends AbstractController(cc)
-    with UserWrites {
+    with UserWrites
+    with ServiceErrorWrites {
 
   def index() = Action.async {
     userService.getAll().map { users =>
@@ -25,6 +26,17 @@ class UserController @Inject()(userService: UserService,
   def show(id: String) = Action.async {
     userService.get(UserId(id)).map {
       _.fold[Result](NotFound)(user => Ok(Json.toJson(user)))
+    }
+  }
+
+  def create() = Action.async(parse.json) { implicit request =>
+    validatedJson[UserCreatingInput] { input =>
+      userService.create(input.name, input.email, input.password).map {
+        _.fold(
+          error => BadRequest(Json.toJson(error)),
+          user => Created(Json.toJson(user))
+        )
+      }
     }
   }
 }
