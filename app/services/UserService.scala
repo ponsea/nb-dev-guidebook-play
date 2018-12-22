78fc66a27884f3d73a3d39c49549e00430b6d11c
ponsea <puodsea@gmail.com>
2018-12-22 20:27:58 +0900

    Add - UserService#create

--- app/services/UserService.scala
+++ app/services/UserService.scala
@@ -17,4 +17,19 @@ class UserService @Inject()(userRepository: UserRepository)(implicit idGen: IdGe
   def get(userId: UserId): Future[Option[User]] = {
     userRepository.findById(userId)
   }
+
+  def create(name: String, email: String, password: String): Future[Either[UserError, User]] = {
+    userRepository.emailExists(email).flatMap {
+      _ match {
+        case true => Future.successful(Left(UserEmailDuplicated(email)))
+        case false => {
+          val salt = crypto.randomString(20)
+          val hashedPassword = crypto.digest(password + salt)
+          val newUser =
+            User(UserId.newId(), name, email, hashedPassword, salt, sdt.now(), sdt.now())
+          userRepository.save(newUser).map(Right(_))
+        }
+      }
+    }
+  }
 }
