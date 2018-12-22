fb4a369a84ef510ff04a29ce4f4ba0e7e993484d
ponsea <puodsea@gmail.com>
2018-12-22 21:02:30 +0900

    Add - User#canEditBy

--- app/models/User.scala
+++ app/models/User.scala
@@ -9,7 +9,9 @@ case class User(id: UserId,
                 hashedPassword: String,
                 salt: String,
                 createdAt: LocalDateTime,
-                updatedAt: LocalDateTime)
+                updatedAt: LocalDateTime) {
+  def canEditBy(editorId: UserId) = this.id == editorId
+}
 
 case class UserId(value: String) extends AnyVal
 
