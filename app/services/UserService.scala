770ff43e427340eae7f0cd2d61f3112641993a16
ponsea <puodsea@gmail.com>
2018-12-22 16:05:35 +0900

    Add - UserService#getAll

---
+++ app/services/UserService.scala
@@ -0,0 +1,16 @@
+package services
+
+import javax.inject.{Inject, Singleton}
+import java.time.LocalDateTime
+import scala.concurrent.{Future, ExecutionContext}
+import utils.{IdGenerator, SystemDateTime, Crypto}
+import persistence.UserRepository
+import models.{User, UserId}
+
+@Singleton()
+class UserService @Inject()(userRepository: UserRepository)(implicit idGen: IdGenerator,
+                                                            sdt: SystemDateTime,
+                                                            crypto: Crypto,
+                                                            ec: ExecutionContext) {
+  def getAll(): Future[Seq[User]] = userRepository.findAll()
+}
