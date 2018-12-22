aca95abe8dc15f261cdf922b1175ba01498e7cad
ponsea <puodsea@gmail.com>
2018-12-22 20:25:10 +0900

    Add - Crypto#randomString

--- app/utils/Crypto.scala
+++ app/utils/Crypto.scala
@@ -2,9 +2,11 @@ package utils
 
 import javax.inject.Singleton
 import java.security.MessageDigest
+import scala.util.Random
 
 trait Crypto {
   def digest(str: String): String
+  def randomString(length: Int): String
 }
 
 @Singleton()
@@ -15,5 +17,9 @@ class CryptoImpl extends Crypto {
       .digest(str.getBytes("UTF-8"))
       .map("%02x".format(_))
       .mkString
+  }
+
+  def randomString(length: Int) = {
+    Random.alphanumeric.take(length).mkString
   }
 }
