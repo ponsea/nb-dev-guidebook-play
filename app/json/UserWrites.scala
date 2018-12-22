ddebbb163b2e00d5d02cdf85c1960caecc26e001
ponsea <puodsea@gmail.com>
2018-12-22 16:06:04 +0900

    Update - デフォルトのUserのWritesを変更

--- app/json/UserWrites.scala
+++ app/json/UserWrites.scala
@@ -1,9 +1,17 @@
 package json
 
-import play.api.libs.json.{Json, Writes, JsString}
+import java.time.LocalDateTime
+import play.api.libs.json.{JsPath, Json, Writes, JsString}
+import play.api.libs.functional.syntax._
 import models.{User, UserId}
 
 trait UserWrites {
   implicit val userIdWrites = Writes[UserId](id => JsString(id.value))
-  implicit val defaultUserWrites = Json.writes[User]
+
+  implicit val defaultUserWrites: Writes[User] = (
+    (JsPath \ "id").write[UserId]
+      and (JsPath \ "name").write[String]
+      and (JsPath \ "createdAt").write[LocalDateTime]
+      and (JsPath \ "updatedAt").write[LocalDateTime]
+  )(user => (user.id, user.name, user.createdAt, user.updatedAt))
 }
