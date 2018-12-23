b6ace7e8329610020b68f74552e692c0aed117ad
ponsea <puodsea@gmail.com>
2018-12-23 13:39:15 +0900

    Update - UserRepositorySlickImplをDIするように

--- app/Module.scala
+++ app/Module.scala
@@ -5,7 +5,7 @@ import utils._
 
 class Module extends AbstractModule {
   def configure() = {
-    bind(classOf[UserRepository]).to(classOf[UserRepositoryOnMemoryImpl])
+    bind(classOf[UserRepository]).to(classOf[UserRepositorySlickImpl])
     bind(classOf[TaskRepository]).to(classOf[TaskRepositorySlickImpl])
     bind(classOf[IdGenerator]).to(classOf[IdGeneratorImpl])
     bind(classOf[SystemDateTime]).to(classOf[SystemDateTimeImpl])
