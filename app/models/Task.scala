75932ae0207b836a67cb944abd997cf8a3f8e124
ponsea <puodsea@gmail.com>
2019-01-11 17:42:13 +0900

    Add - Task#updated

--- app/models/Task.scala
+++ app/models/Task.scala
@@ -1,7 +1,7 @@
 package models
 
 import java.time.LocalDateTime
-import utils.IdGenerator
+import utils.{IdGenerator, SystemDateTime}
 
 case class Task(id: TaskId,
                 name: String,
@@ -10,7 +10,14 @@ case class Task(id: TaskId,
                 deadline: Option[LocalDateTime],
                 createdAt: LocalDateTime,
                 updatedAt: LocalDateTime) {
-  def canEditBy(editorId: UserId) = this.userId == editorId
+  def canEditBy(editorId: UserId): Boolean = this.userId == editorId
+
+  def updated(
+      name: String = this.name,
+      isFinished: Boolean = this.isFinished,
+      deadline: Option[LocalDateTime] = this.deadline)(implicit sdt: SystemDateTime): Task = {
+    this.copy(name = name, isFinished = isFinished, deadline = deadline, updatedAt = sdt.now())
+  }
 }
 
 case class TaskId(value: String) extends AnyVal
