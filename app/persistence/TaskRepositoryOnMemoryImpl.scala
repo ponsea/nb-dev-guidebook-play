4365f7f78dfca322858f5daf3923bc13b3ca8e1f
ponsea <puodsea@gmail.com>
2019-01-11 13:30:06 +0900

    Update - リポジトリのオンメモリ実装はコンストラクタで初期データを設定できるように

--- app/persistence/TaskRepositoryOnMemoryImpl.scala
+++ app/persistence/TaskRepositoryOnMemoryImpl.scala
@@ -1,20 +1,12 @@
 package persistence
 
 import javax.inject.Singleton
-import java.time.LocalDateTime
 import scala.concurrent.Future
-import models.{Task, TaskId, UserId}
+import models.{Task, TaskId}
 
 @Singleton()
-class TaskRepositoryOnMemoryImpl extends TaskRepository {
-  private val dateTime = LocalDateTime.parse("2019-04-01T00:00:00")
-  private var tasks = Set(
-    Task(TaskId("1"), "Scalaの勉強", true, UserId("1"), Some(dateTime), dateTime, dateTime),
-    Task(TaskId("2"), "Playの勉強", false, UserId("1"), Some(dateTime), dateTime, dateTime),
-    Task(TaskId("3"), "英語の勉強", false, UserId("1"), None, dateTime, dateTime),
-    Task(TaskId("4"), "たまご買う", true, UserId("2"), Some(dateTime), dateTime, dateTime),
-    Task(TaskId("5"), "ねぎ買う", false, UserId("2"), Some(dateTime), dateTime, dateTime)
-  )
+class TaskRepositoryOnMemoryImpl(initialData: Set[Task] = Set.empty) extends TaskRepository {
+  private var tasks = initialData
 
   def findAll() = Future.successful(tasks.toSeq)
 
@@ -23,8 +15,7 @@ class TaskRepositoryOnMemoryImpl extends TaskRepository {
   }
 
   def save(newTask: Task) = {
-    val maybeOldTask = tasks.find(_.id == newTask.id)
-    maybeOldTask.foreach(old => tasks -= old)
+    tasks.find(_.id == newTask.id).foreach(old => tasks -= old)
     tasks += newTask
     Future.successful(newTask)
   }
